9635c3940a0229bba9d26346255d992f603e7ae8
Victor Minchev <victor.minchew@gmail.com>
2023-01-28 17:45:58 +0200

    Change assets with dynamic

---
+++ app/Providers/LogoProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\LogoSettings;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class LogoProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $logo = cache()->remember(
+            key:'settings',
+            ttl:3600,
+            callback: fn() => LogoSettings::all()->keyBy('key')
+        );
+        View::share('logo', $logo);
+    }
+}
